37005426be7fc9100571f8034c0d4d876c5c3a6d
Krzysztof Begiedza <8686134+kbegiedza@users.noreply.github.com>
2022-01-06 15:52:59 +0100

    Added docker group

--- fresh_install.sh
+++ fresh_install.sh
@@ -56,6 +56,9 @@ function install_dev_tools() {
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
          $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+    sudo groupadd docker &&
+    sudo usermod -aG docker $USER &&
+    newgrp docker
     sudo apt-get update &&
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io &&
     sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
