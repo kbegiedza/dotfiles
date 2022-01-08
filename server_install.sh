d2ea71d09d57454e4b211d2b4f2be0377ee9875b
Krzysztof Begiedza <contact@kbegiedza.eu>
2022-01-08 13:57:42 +0100

    fixed missing command

--- server_install.sh
+++ server_install.sh
@@ -35,11 +35,12 @@ function install_dependencies() {
         curl \
         gnupg2 \
         xclip &&
+    rm /usr/share/keyrings/docker-archive-keyring.gpg &&
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg &&
     echo \
         "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
         $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null &&
-    sudo update &&
+    sudo apt update &&
     sudo apt install -y \
         docker-ce \
         docker-ce-cli \
