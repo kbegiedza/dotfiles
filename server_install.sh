545452905b653db8915873a6cfff0cda4f9ec1d0
Krzysztof Begiedza <8686134+kbegiedza@users.noreply.github.com>
2022-08-22 22:06:28 +0200

    Update server_install.sh

--- server_install.sh
+++ server_install.sh
@@ -36,7 +36,7 @@ function install_dependencies() {
         gnupg2 \
         xclip \
 	net-tools &&
-    rm /usr/share/keyrings/docker-archive-keyring.gpg &&
+    rm -f /usr/share/keyrings/docker-archive-keyring.gpg &&
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg &&
     echo \
         "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
