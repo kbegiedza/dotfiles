8473e9efb049426780510aa823c5243aaff31435
Krzysztof Begiedza <contact@kbegiedza.eu>
2025-07-17 19:25:26 +0200

    Enhance installation script with start and end messages

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+echo "[dotfiles] Installation started."
+
 git config --global user.name "Krzysztof Begiedza"
 git config --global user.email "contact@kbegiedza.eu"
 git config --global init.defaultBranch master
+
+echo "[dotfiles] Installation done."
+echo "^^Happy hacking^^"
