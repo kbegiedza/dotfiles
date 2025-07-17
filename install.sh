9ff51afc70d3f9fa7365945e94522ec1e0d70c68
Krzysztof Begiedza <contact@kbegiedza.eu>
2025-07-17 22:17:46 +0200

    v++

--- install.sh
+++ install.sh
@@ -1,10 +1,33 @@
 #!/bin/sh
 
-echo "[dotfiles] Installation started."
+set -e
 
-git config --global user.name "Krzysztof Begiedza"
-git config --global user.email "contact@kbegiedza.eu"
-git config --global init.defaultBranch master
+set_system_preferences() {
+  echo "[set_system_preferences] Setting up system preferences"
 
-echo "[dotfiles] Installation done."
+  scutil --set HostName ulfmac
+  scutil --set ComputerName ulfmac
+  scutil --set LocalHostName ulfmac
+
+  echo "[set_system_preferences] Installing fonts"
+  brew install --cask font-jetbrains-mono
+}
+
+initialize_git() {
+  echo "[initialize_git] Initializing git configuration"
+
+  git config --global user.name "Krzysztof Begiedza"
+  git config --global user.email "contact@kbegiedza.eu"
+  git config --global init.defaultBranch master
+}
+
+echo "[dotfiles] Installation started"
+
+set_system_preferences
+initialize_git
+
+brew install gpg
+
+
+echo "[dotfiles] Installation done"
 echo "^^Happy hacking^^"
